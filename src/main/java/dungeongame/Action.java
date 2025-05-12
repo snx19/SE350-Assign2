0d816ca7fddc57bb329a73db1b3684f58d595d00
snx19 <hayato@msnx.local>
2025-05-12 18:47:32 -0500

    created and added code to Action interface

--- src/main/java/dungeongame/Action.java
+++ src/main/java/dungeongame/Action.java
@@ -1,4 +1,20 @@
 package dungeongame;
 
-public class Action {
+/**
+ * Represents an action the player takes
+ */
+public interface Action {
+
+    /**
+     * Executes action on dungeon
+     * @param dungeon Dungeon where action occurs
+     */
+    void execute(Dungeon dungeon);
+
+    /**
+     * Returns a string that describes the action
+     * @return Description of action
+     */
+    @Override
+    String toString();
 }
