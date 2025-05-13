8b967d29753ff53b5eaf1de7f5d74c467b8c5af1
snx19 <hayato@msnx.local>
2025-05-12 21:57:37 -0500

    added new logic in Fight.java

--- src/main/java/dungeongame/Fight.java
+++ src/main/java/dungeongame/Fight.java
@@ -9,7 +9,33 @@ public class Fight implements Action {
 
     @Override
     public void execute(Dungeon dungeon) {
+        Character player = dungeon.getPlayer();
+        Monster monster = door.getMonster();
+
+        if (monster == null) {
+            System.out.println("There is no monster");
+            return;
+        }
         System.out.println("Fight: " + door.getMonster().getName());
+
+        while (player.getHealth() > 0 && monster.getHealth() >0) {
+            monster.takeDamage(player.getStrength());
+            System.out.println(monster.getName() + " has " + monster.getHealth() + " health");
+
+            if (monster.getHealth() <= 0) {
+                System.out.println(monster.getName() + " has been defeated");
+                return;
+            }
+
+            player.takeDamage(monster.getStrength());
+            System.out.println(player.getName() + " has " + player.getHealth() + " health");
+
+            if (player.getHealth() <= 0) {
+                System.out.println(player.getName() + " has been defeated");
+                return;
+            }
+        }
+
     }
 
     @Override
